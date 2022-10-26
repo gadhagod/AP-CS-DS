b5ae65a25fe6f4cf0b06c30ac6d1960bb3db170a
gadhagod <69025547+gadhagod@users.noreply.github.com>
2022-10-25 17:06:42 -0700

    Add ListsLab (MyArrayList implementation)

---
+++ ListsLab/MyArrayList.java
@@ -0,0 +1,335 @@
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+/*
+ * My implementation of an ArrayList
+ * @author Aarav Borthakur
+ * @version Oct 22, 2022
+ */
+public class MyArrayList<E>
+{
+	private int size;
+	private Object[] values; 
+	private boolean modified; // Whether elements have been added/removed
+
+	/*
+	 * Creates a MyArrayList
+	 */
+	public MyArrayList()
+	{
+		size = 0;
+		values = new Object[1];
+	}
+
+	/**
+	 * Gets a string representation of this MyArrayList
+	 * @return	The string representation of this list
+	 */
+	public String toString()
+	{
+		if (size == 0)
+			return "[]";
+
+		String s = "[";
+		for (int i = 0; i < size - 1; i++)
+			s += values[i] + ", ";
+		return s + values[size - 1] + "]";
+	}
+
+	/**
+	* Doubles the capapacity of the list
+	* @postcondition replaces the array with one that is
+	*               twice as long, and copies all of the
+	*               old elements into it
+	*/
+	private void doubleCapacity()
+	{
+		Object[] newValues = new Object[values.length  * 2];
+		for (int i = 0; i < values.length; i++)
+		{
+			newValues[i] = values[i];
+		}
+		values = newValues;
+	}
+
+	/**
+	* Gets the capacity of this list
+	* @return 		 The capacity of the list
+	* @postcondition returns the length of the array
+	*/
+	public int getCapacity()
+	{
+		return values.length;
+	}
+
+	/**
+	 * Gets the size of the list
+	 * @return 		  The current size of the list
+	 */
+	public int size()
+	{
+		return size;
+	}
+
+	/**
+	 * Gets the value in the list at an index
+	 * @param index		The index of the item to be retrieved
+	 * @return 			The value at index
+	 */
+	public E get(int index)
+	{
+		return (E) values[index];
+	}
+
+	/** 
+	* Replaces an element at a position
+	* @param index		The index at which to place the replacement
+	* @param obj		The item to place into the list
+	* @return 			The previous value at index
+	* @postcondition replaces the element at position index with obj
+	*               returns the element formerly at the specified position
+	*/
+	public E set(int index, E obj)
+	{
+		Object prev = values[index];
+		values[index] = obj;
+		return (E) prev;
+	}
+
+	/**
+	* Adds an item at an index 
+	* @param obj 	 Adds an item to the end of the list
+	* @return 		 true
+	* @postcondition appends obj to end of list; returns true
+	*/
+	public boolean add(E obj)
+	{
+		modified = true;
+		if(getCapacity() == size) 
+		{
+            doubleCapacity();
+        }
+        add(size, obj);
+        return true;
+	}
+
+	/**
+	* Removed an item from the list given an index
+	* @param index	 The index of the item to be removed
+	* @return		 The object that was removed
+	* @postcondition removes element from position index, moving elements
+	*               at position index + 1 and higher to the left
+	*               (subtracts 1 from their indices) and adjusts size
+	*               returns the element formerly at the specified position
+	*/
+	public E remove(int index)
+	{
+		modified = true;
+		Object temp = values[index];
+        for(int i = index + 1; i < getCapacity(); i++) 
+		{
+            values[i - 1] = values[i];
+        }
+        size--;
+        return (E) temp;
+	}
+
+	public Iterator<E> iterator()
+	{
+		return new MyArrayListIterator();
+	}
+
+	/**
+	* Adds an item at index obj
+	* @param index	 The index at which to add obj
+	* @param obj	 The object ot be added
+	* @precondition  0 <= index <= size
+	* @postcondition inserts obj at position index,
+	*               moving elements at position index and higher
+	*               to the right (adds 1 to their indices) and adjusts size
+	*/
+	public void add(int index, E obj)
+	{
+		if(size >= values.length) {
+            doubleCapacity();
+        }
+        for(int i = index + 1; i < values.length; i++) {
+            values[i]=values[i-1];
+        }
+        values[index] = obj;
+        size++;
+	}
+
+	/**
+	 * MyArrayListIterator is a unidirectional iterator that can remove values
+	 * @author Aarav Borthakur
+	 * @version October 22, 2022 
+	 */
+	private class MyArrayListIterator implements Iterator<E>
+	{
+		//the index of the value that will be returned by next()
+		private int nextIndex;
+
+		/**
+		 * Creates a MyArrayListIterator
+		 */
+		public MyArrayListIterator()
+		{
+			nextIndex = 0;
+		}
+		/**
+		 * Returns whether there is a next value in the list
+		 * @return 		Whether there is a next value in the list
+		 */
+		public boolean hasNext()
+		{
+			return nextIndex < size && values[nextIndex] != null;
+		}
+
+		/**
+		 * Gets the next item in the list 
+		 * @return 		  The next value in the list
+		 * @postcondition Moves the index up by one
+		 */
+		public E next()
+		{
+			Object temp = values[nextIndex];
+            nextIndex++;
+            return (E) temp;
+		}
+
+		/*
+		 * Removes the last accessed value from the list
+		 * @postcondition removes the last element that was returned by next
+		 */
+		public void remove()
+		{
+			MyArrayList.this.remove(nextIndex - 1);
+		}
+	}
+
+	/**
+	 * MyArrayListListIterator is a bidirectional iterator that can add and remove elements
+	 */
+	private class MyArrayListListIterator extends MyArrayListIterator implements ListIterator<E>
+	{
+		private int nextIndex;
+		private int previousIndex;// Index of element that will be returned by the next call of next()
+		private boolean forward; //direction of traversal
+
+		/**
+		 * Constructs a new MyArrayListListIterator
+		 */
+		public MyArrayListListIterator()
+		{
+			nextIndex = 0;
+			previousIndex = -1;
+			forward = true;
+		}
+		
+		/**
+		 * 
+		 * Returns next element in the list
+		 * @postcondition 	moves pointer forward
+		 * @return 			next Object in MyArryaList
+		 */
+		public E next()
+		{
+			if (modified)
+			{
+				throw new ConcurrentModificationException();
+			}
+			Object val = values[nextIndex];
+			nextIndex++;
+			previousIndex++;
+			forward = true;
+			return (E) val;
+		}
+
+		/**
+		 * Adds element before element that would be returned by next
+		 * @param element to add
+		 * @postcondition an element is added to the list
+		 */
+		public void add(E obj)
+		{
+			MyArrayList.this.add(nextIndex, obj);
+		}
+
+		/**
+		 * Determines whether there is another element in MyArrayList
+		 * while traversing in the backward direction
+		 * @return true if there is another element, false otherwise
+		 */
+		public boolean hasPrevious()
+		{
+			return previousIndex >= 0;
+		}
+
+		/**
+		 * Retrieves the previous item in the list
+		 * @return 		The previous item in the list
+		 */
+		public E previous()
+		{
+			forward = false;
+			nextIndex--;
+			previousIndex--;
+			return get(nextIndex);
+		}
+
+		/**
+		 * Returns index of the next element 
+		 * @return index of element that would be 
+		 *         returned by a call to next()
+		 */
+		public int nextIndex()
+		{
+			return nextIndex;
+		}
+
+		/**
+		 * Gets the index of the previous element
+		 * @return		The index of the previous index
+		 */
+		public int previousIndex()
+		{
+			return previousIndex;
+		}
+
+		/**
+		 * Removes element that was returned by next() pr previous()
+		 * USE direction FOR THIS
+		 * @postcondition		an item is removed from the list
+		 */
+		public void remove()
+		{
+			if (forward)
+			{
+				MyArrayList.this.remove(previousIndex);
+			}
+			else 
+			{
+				MyArrayList.this.remove(nextIndex);
+			}
+		}
+
+		/**
+		 * Sets the next item 
+		 * @param obj		The object to set the next to
+		 * @postcondition	The next item is set to obj
+		 */
+		public void set(E obj)
+		{
+			if (forward)
+			{
+				MyArrayList.this.set(previousIndex, obj);
+			}
+			else 
+			{
+				MyArrayList.this.set(nextIndex, obj);
+			}
+		}
+	}
+}
---
+++ ListsLab/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] aStrings) {
+        MyArrayList x = new MyArrayList<Integer>();;
+        x.add(95);
+        System.out.println(x.getCapacity());
+
+        System.out.println(x);
+    }
+}
