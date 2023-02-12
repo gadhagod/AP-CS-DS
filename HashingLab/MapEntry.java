e59fc4e4d448f6ebee5086e111651292559b75df
gadhagod <69025547+gadhagod@users.noreply.github.com>
2023-02-12 12:19:41 -0800

    HashingLab: Document MapEntry.java

--- HashingLab/MapEntry.java
+++ HashingLab/MapEntry.java
@@ -1,7 +1,9 @@
 import java.util.Map.Entry;
 
 /**
- * 
+ * MapEntry represents a single
+ * entry in a map consisting of
+ * a key and a value
  * @author joelmanning
  * @version 1.0
  */
@@ -11,10 +13,10 @@ public class MapEntry<K, V> implements Entry<K, V>
     private V value;
 
     /**
-     * @param key
-     *            the initial key for the entry
-     * @param value
-     *            the initial value for the entry
+     * Constructs a MapEntry given its key and
+     * the value they key maps to
+     * @param key       the key for the entry
+     * @param value     the initial value for the entry
      */
     public MapEntry(K key, V value)
     {
@@ -24,7 +26,8 @@ public class MapEntry<K, V> implements Entry<K, V>
     }
 
     /**
-     * @return the key in the entry
+     * Gets the key of this MapEntry
+     * @return      the key in the entry
      */
     public K getKey()
     {
@@ -32,8 +35,8 @@ public class MapEntry<K, V> implements Entry<K, V>
     }
 
     /**
-     * @param key
-     *            the key to set this entry's key to
+     * Sets the key of this MapEntry
+     * @param key   the key to set this entry's key to
      */
     public void setKey(K key)
     {
@@ -41,6 +44,7 @@ public class MapEntry<K, V> implements Entry<K, V>
     }
 
     /**
+     * Gets the value of this MapEntry
      * @return the value of the entry
      */
     public V getValue()
@@ -49,9 +53,9 @@ public class MapEntry<K, V> implements Entry<K, V>
     }
 
     /**
-     * @return the previous value in the entry
-     * @param val
-     *            the value to set this entry's value to
+     * Sets the value of this MapEntry
+     * @return      the previous value in the entry
+     * @param val   the value to set this entry's value to
      */
     public V setValue(V val)
     {
@@ -61,9 +65,11 @@ public class MapEntry<K, V> implements Entry<K, V>
     }
 
     /**
-     * @param me
-     *            the MapEntry to check if this is equal to
-     * @return whether it is equal to that MapEntry
+     * Checks whether another MapEntry is equal to this
+     * MapEntry by testing if their keys are equal with
+     * the key's equals() method
+     * @param me    the MapEntry to check if this is equal to
+     * @return      whether it is equal to that MapEntry
      */
     public boolean equals(MapEntry me)
     {
